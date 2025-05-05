ddc079fd223c8746ce087ad5fb6be7163f6e6a59
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-05-04 23:17:21 -0300

    feat: add jwt token generation endpoint

--- jwt-express-maheshkariya/index.js
+++ jwt-express-maheshkariya/index.js
@@ -2,8 +2,24 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 
 const app = express();
-
 const port = 3000;
+
+app.use(express.json());
+
+app.get("/api/token", (req, res) => {
+  const { name, email } = req.body;
+  const payload = {
+    name,
+    email,
+  };
+
+  const secret = "mySecret";
+  const token = jwt.sign(payload, secret, { expiresIn: "1h" });
+
+  res.status(200).json({
+    token,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
