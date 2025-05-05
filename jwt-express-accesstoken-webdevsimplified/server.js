7bc023573e077d2c41d4b8d440642bec23bc143d
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-05-05 10:07:53 -0300

    feat: add user registration route with password hashing and jwt generation

--- jwt-express-accesstoken-webdevsimplified/server.js
+++ jwt-express-accesstoken-webdevsimplified/server.js
@@ -1,8 +1,30 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-app.get('/', (req, res) => {});
+const app = express();
+app.use(express.json());
+
+const users = [{ username: 'Admin', password: '12345' }];
+
+const posts = [
+  { username: 'Kyle', title: 'Post 1' },
+  { username: 'Kyle', title: 'Post 2' },
+];
+
+app.get('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  users.push({ username, password: hashedPassword });
+
+  const accessToken = jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '15m',
+  });
+
+  res.status(201).json({ accessToken: accessToken });
+});
 
 app.listen(3000, () => {
-  console.log(`Server running on port 3000}`);
+  console.log(`Server running on port 3000`);
 });
