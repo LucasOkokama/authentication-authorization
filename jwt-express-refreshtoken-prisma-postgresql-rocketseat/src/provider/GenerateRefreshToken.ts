2fe596f781876c43055a85a4f998f91ea16c019b
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-05-07 17:43:33 -0300

    feat: create generate token provider

--- jwt-express-refreshtoken-prisma-postgresql-rocketseat/src/provider/GenerateRefreshToken.ts
+++ jwt-express-refreshtoken-prisma-postgresql-rocketseat/src/provider/GenerateRefreshToken.ts
@@ -3,6 +3,7 @@ import { client } from '../prisma/client';
 
 class GenerateRefreshToken {
   async execute(userId: string) {
+    console.log(userId);
     const expiresIn = dayjs().add(15, 'second').unix();
 
     const generateRefreshToken = await client.refreshToken.create({
