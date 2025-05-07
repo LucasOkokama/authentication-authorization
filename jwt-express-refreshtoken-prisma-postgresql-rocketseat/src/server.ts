9cb8484e73abcc74a759bf0e167ecfc9c9a271e6
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-05-07 12:14:42 -0300

    feat: add global error handling middleware

--- jwt-express-refreshtoken-prisma-postgresql-rocketseat/src/server.ts
+++ jwt-express-refreshtoken-prisma-postgresql-rocketseat/src/server.ts
@@ -1,10 +1,21 @@
-import express, { json } from 'express';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    response.json({
+      status: 'Error',
+      message: error.message,
+    });
+    return;
+  }
+);
 
 app.listen(3000, () => {
   console.log(`Server is running or port 3000`);
