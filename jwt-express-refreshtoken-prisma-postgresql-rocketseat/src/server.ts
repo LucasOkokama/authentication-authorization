5d7d575cc9d5b989409d49219fd0e87d1fde397f
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-05-07 11:52:48 -0300

    feat: create user use case and controller with prisma integration

--- jwt-express-refreshtoken-prisma-postgresql-rocketseat/src/server.ts
+++ jwt-express-refreshtoken-prisma-postgresql-rocketseat/src/server.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express, { json } from 'express';
+import { router } from './routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => {
   console.log(`Server is running or port 3000`);
